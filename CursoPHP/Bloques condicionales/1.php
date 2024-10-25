59da77b9fec1e09e8138a24704d7fa8455950983
PruebaJonaClase <franciagonzalez23@iespabloserrano.com>
2024-10-25 18:29:10 +0200

    segundo vez

---
+++ CursoPHP/Bloques condicionales/1.php
@@ -0,0 +1,14 @@
+<?php
+$numero = rand(-10, 10);
+echo "el numero elegido es $numero <br/>";
+
+if($numero>0){
+    echo "es positivo";
+}
+elseif($numero==0){
+    echo "el numero es 0";
+}
+else{
+    echo "es negativo";
+}
+?>
---
+++ CursoPHP/Prueba formulario/procesar.php
@@ -0,0 +1,12 @@
+<?php
+$numero = rand(-10, 10);
+echo "el numero elegido es $numero <br/>";
+
+if($numero>=0){
+    echo "es positivo";
+}
+
+else{
+    echo "es negativo";
+}
+?>
