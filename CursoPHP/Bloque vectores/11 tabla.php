d054b7171cb76e84481b157c8c7d84ed135760a3
PruebaJonaClase <franciagonzalez23@iespabloserrano.com>
2024-10-31 20:50:55 +0100

    a

--- CursoPHP/Bloque vectores/11 tabla.php
+++ CursoPHP/Bloque vectores/11 tabla.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" src="estilos.css">
+</head>
+<body>
+
 <?php
 
 $array1 = array("Coche"=>"2", "Escoba"=>"101", "Casa"=>"21", "Mesa"=>"7890", "Sofa"=>"92");
@@ -12,3 +22,5 @@ foreach($array1 as $producto => $precio){
 }
 echo "</table>";
 ?>
+</body>
+</html>
