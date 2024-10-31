62ada9a3a2f684f53707594e34467f8c244db96c
PruebaJonaClase <franciagonzalez23@iespabloserrano.com>
2024-10-31 18:36:33 +0100

    otro

--- CursoPHP/Bloque loops/7.php
+++ CursoPHP/Bloque loops/7.php
@@ -1,10 +1,21 @@
 <?php
-$contador = 0;
+$numero = rand (1,30);
 
-while($contador < 20){
-    $contador ++;
-    $numero = rand(1,6);
-    echo "$numero <br/>";
+$primo = [];
+for($contador = 1; $contador <= $numero; $contador ++){
+    if($numero % $contador == 0){
+        //echo "$contador <br/>";
+        $primo[] = $contador;
+    }
 }
+echo "$numero <br/>";
+if(count($primo) == 2){
+    echo "es un numero primo";
+
+}
+else{
+    echo "no es un numero primo";
+}
+
 
 ?>
